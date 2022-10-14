1c0603555f8396cea1f3d8b728b27c68f24ab7b1
JHTNT <jerry1106666@gmail.com>
2022-10-14 12:16:46 +0800

    Fix: completed version

--- Ch05_11.java
+++ Ch05_11.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Ch05_11 {
@@ -5,14 +7,34 @@ public class Ch05_11 {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt();
         int c = sc.nextInt(), d = sc.nextInt(), e = sc.nextInt();
+        int g = -1, h = -1, i = -1;
         sc.close();
 
+        HashSet<Integer> num = new HashSet<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        num.remove(a); num.remove(b); num.remove(c);
+        num.remove(d); num.remove(e);
+
         // abc + gch = eeid
-        // g0h = 1108 - 763 - 30 - i * 10
-        int tmp = e * 1100 + d - a * 100 - b * 10 - c * 11;
-        int g = tmp / 100, i = tmp % 100 / 10, h = tmp % 10;
-        System.out.println(g);
-        System.out.println(h);
-        System.out.print(i);
+        int left = a * 100 + b * 10 + c * 11;
+        int right = e * 1100 + d;
+        for (int x : num) {
+            for (int y : num) {
+                if (y == x) continue;
+                for (int z : num) {
+                    if (z == x || z == y) continue;
+                    if (left + x * 100 + y == right + z * 10) {
+                        g = x; h = y; i = z;
+                    }
+                }
+            }
+        }
+
+        if (g == -1) {
+            System.out.println(g);
+        } else {
+            System.out.println(g);
+            System.out.println(h);
+            System.out.print(i);
+        }
     }
 }
